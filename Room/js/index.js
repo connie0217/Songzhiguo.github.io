d565ccac08b36e589346db2f429b275cc8dffc66
Songzhiguo <Fu_shide@163.com>
2017-03-23 00:52:09 +0800

    Room1

---
+++ Room/js/index.js
@@ -0,0 +1,305 @@
+"use strict";
+$(function() {
+    var $bgbox = $("#jsBgBox");//背景盒子
+    var dialogFlag = true;//会话框开关,点击时为true才能消失
+    var key1 = 0, key2 = 0, key3 = 0, key4 = 0;//四把钥匙
+    var $dialog = $("#dialogBox");//对话框
+    var $dialogKey = $dialog.find(".dialog-key");//对话框线索
+    var $dialogTips = $(".dialog-tips");//对话框提示层
+    var $dialogTitle = $dialogTips.find("h3");//data-title
+    var $dialogDisc = $dialogTips.find("p");//data-disc
+    var lightIsOpened = "true";//true为开灯
+    var isFinished = false;//完成线索
+    var isFromDoor = false;//输入密码开关
+    var addShakeEvent = function() {
+        var ShakeEvent = new Shake({
+            threshold: 15,
+            timeout: 1e3
+        });
+        ShakeEvent.start();
+        window.addEventListener("shake", shakeEventDidOccur, false);
+    };
+    var removeShakeEvent = function() {
+        window.removeEventListener("shake", shakeEventDidOccur, false);
+    };
+    //点击房间内的物品
+    $(".item").on("tap", function(){
+        // $dialog.attr('class','item show');
+        var $this = $(this);//点击的物品
+        //获取点击$this的属性值
+        var type = $this.attr("data-type");
+        var title = $this.attr("data-title");
+        var disc = $this.attr("data-disc");
+        var isKey = $this.attr("data-iskey");
+        var img = $this.attr("data-img");
+        //点击$this的type不为2，$this属性添加after
+        if (type != 2){
+            $(this).addClass("after");
+        }
+        isFromDoor = false;
+        //添加延迟定时器，点击$this500毫秒后执行
+        setTimeout(function(){
+            if (type == 1) {//点击$this的自定义属性type为1
+                if (!lightIsOpened) {//!lightIsOpened为关灯状态下
+                    showDialog("太暗了，看不太清楚", "打开灯来吧", "05", "false");//调用showDialog
+                } else {//开灯状态下
+                    if ($this.attr("id") == "door" && isFinished == true) {
+                        isFromDoor = true;
+                        showResult();
+                    } else {
+                        showDialog(title, disc, img, isKey);
+                    }
+                }
+            } else if (type == 2) {//点击$this的type为2
+                if (!lightIsOpened) {//关灯状态
+                    showDialog("太暗了，看不太清楚", "打开灯来吧", "05", "false");
+                } else {
+                    return false;
+                }
+            } else if (type == 3) {//点击$this的type为3
+                var Tips = $this.attr("data-tips");//获取点击$this的data-tips属性值
+                if (!lightIsOpened) {//关灯状态
+                    showDialog("太暗了，看不太清楚", "打开灯来吧", "05", "false");
+                } else {//开灯状态
+                    showDialog(title, disc, img, isKey, Tips);
+                }
+            } else if (type == 4) {} else if (type == 5) {
+                if (!lightIsOpened) {//关灯状态
+                    showDialog("太暗了，看不太清楚", "打开灯来吧", "05", "false");
+                } else {//开灯状态
+                    showDialog(title, disc, img, isKey);
+                }
+            }
+            //播放当前触发事件的背景音乐
+            if ($this.attr("id") == "poster") {
+                $("#audioPoster")[0].play();
+            } else if ($this.attr("id") == "trash") {
+                $("#audioTrash")[0].play();
+            } else if ($this.attr("id") == "video") {
+                $("#audioVideo")[0].play();
+            } else if ($this.attr("id") == "light") {
+                $("#audioLight")[0].play();
+            }
+        }, 500);
+    });
+    //按下开关
+    $("#light").on("touchstart", function() {
+        $bgbox.toggleClass("dark");//按下开关$bgbox的class属性中没有dark就添加，有就移出dark
+        $(".item").toggleClass("hide");//按下开关$(".item")的class属性中没有hide就添加，有就移出hide
+        //按下开关改变灯的开关状态，用于辨别是否在开灯状态下点击
+        if (lightIsOpened) {
+            lightIsOpened = false;
+        } else {
+            lightIsOpened = true;
+            // $bgbox.removeClass("dark");
+        }
+    });
+    var lightAIsOpened = false;//床头灯开关 false为关灯状态
+    var lightBIsOpened = false;//台灯开关   false为开灯状态
+    //按下房间内的台灯、床头灯
+    $(".item").on("touchstart", function() {
+        if ($(this).hasClass("light_a")) {//床头灯
+            if (lightAIsOpened) {//床头灯开关为true，开灯状态
+                lightAIsOpened = false;//改变床头灯开关状态为false
+                $(this).removeClass("after");//移出after，关灯
+            } else {//床头灯开关为false,关灯状态
+                lightAIsOpened = true;//改变床头灯开关状态为true
+                $(this).addClass("after");//添加after，开灯
+            }
+        } else if ($(this).hasClass("light_b")) {//台灯
+            if (lightBIsOpened) {//台灯开关状态为true,关灯状态
+                lightBIsOpened = false;//改变台灯开关状态
+                $(this).removeClass("after");//移出after,开灯
+            } else {//台灯开关为false,开灯状态
+                lightBIsOpened = true;//改变台灯开关状态为true
+                $(this).addClass("after");//移出after,关灯
+            }
+        }
+    });
+    //按下提示遮罩移出提示遮罩节点
+    $(".page-tips").on("touchstart", function() {
+        $(this).remove();
+    });
+    var showResult = function() {
+        var count = key1 + key2 + key3 + key4;//已获得总线索
+        var $progress = $(".dialog-progress span");//获得线索
+        var title = "", disc = "", img = "";
+        $progress.html(count);//改变已获得线索
+        if (count == 1) {//获得1条线索
+            title = "获得一块“记忆碎片”! ";//设置title
+            disc = "上面好像有奇怪的图案</br>好像只是其中一部分";//设置disc
+            img = "06";//显示图片序号
+            showDialog(title, disc, img, "false");//传入参数
+        } else if (count == 2) {
+            title = "获得第二块“记忆碎片”！ ";
+            disc = "两块碎片叠在一起了！</br>图案还是看不太清";
+            img = "07";
+            showDialog(title, disc, img, "false");
+        } else if (count == 3) {
+            title = "获得第三块“记忆碎片”！";
+            disc = "把碎片叠到一起</br>看起来是些数字";
+            img = "08";
+            showDialog(title, disc, img, "false");
+        } else if (count == 4) {
+            title = "获得最后一块“记忆碎片”！ ";
+            disc = "终于完成了！“0325”！</br>快去开门解锁吧！";
+            img = "09";
+            isFinished = true;//完成线索
+            showDialog(title, disc, img, "false");
+            $dialog.on("tap", function() {
+                if (isFromDoor) {
+                    $(".lock-key").addClass("show");//显示输入密码
+                }
+            });
+        }
+    };
+    var showDialog = function(dTitle, dDisc, dImg, isKey, dTips) {
+        $dialog.addClass("show");//会话框显示
+        var $mask = $dialog.find(".dialog-mask");//点击查看
+        var $audioResult = $("#audioResult")[0];//成功拿到线索音频
+        if (dImg !== "none") {//自定义属性data-img ！= "none";
+            $dialogKey.removeClass("hide");//删除class属性hide
+            $dialogKey.find(".key-img").removeClass().addClass("key-img key-img_" + dImg);
+            $dialogKey.find(".key-tips").html(dTips);//改变key-tips的innerHTML内容
+        } else {//
+            $dialogKey.addClass("hide");//添加class属性hide
+        }
+        if (isKey == "true"){
+            dialogFlag = false;
+        } else {
+            dialogFlag = true;
+        }
+        $dialogTitle.html(dTitle);//改变$dialogTitled的innerHTML内容
+        $dialogDisc.html(dDisc);//改变$dialogDisc的innerHTML内容
+        $(".key-img span").show();//显示需要点击的span
+        if (dImg == 1) {//自定义属性dImg为01
+            if (dTips == "您已经拿到该线索！") {//已拿到该线索
+                dialogFlag = true;
+                $(".key-img").addClass("result");//切换拿到线索的图片
+                $(".key-img span").hide();//需要点击的线索span隐藏
+                $dialogTitle.html("诶，打开里面居然有东西！");//改变$dialogTitle的innerHTML内容
+                $dialogDisc.html("");//清空$dialogDisc的innerHTML内容
+            } else {//该线索会话框首次显示
+                $(".key-img_01 span").on("tap", function() {//点击该线索span
+                    $(".key-img_01").addClass("result");//切换拿到线索的图片
+                    $(".key-img span").hide();//需要点击的线索span隐藏
+                    $dialogTitle.html("诶，打开里面居然有东西！");//改变$dialogTitle的innerHTML内容
+                    $dialogDisc.html("");//清空$dialogDisc的innerHTML内容
+                    $dialogKey.find(".key-tips").html("");//清空$key-tips的innerHTML内容
+                    key1 = 1;//拿到该线索，key1=1
+                    $("#key01").attr("data-tips", "您已经拿到该线索！");//设置该线索的自定义属性
+                    $mask.addClass("show");//显示点击查看按钮
+                });
+            }
+        } else if (dImg == 2) {//自定义属性dImg为02
+            if (dTips == "您已经拿到该线索！") {//已拿到该线索
+                dialogFlag = true;
+                $(".key-img").addClass("result");//切换拿到线索的图片
+                $(".key-img span").hide();//需要点击的线索span隐藏
+                $dialogTitle.html("原来上一次下副本<br>已经过了那么久了");//改变$dialogTitle的innerHTML内容
+                $dialogDisc.html("");//清空$dialogDisc的innerHTML内容
+            } else {//该线索会话框首次显示
+                $(".key-img_02 span").on("tap", function() {//点击该线索span
+                    $(".key-img_02").addClass("result");//切换拿到线索的图片
+                    $(".key-img span").hide();//需要点击的线索span隐藏
+                    $dialogTitle.html("原来上一次下副本<br>已经过了那么久了");//改变$dialogTitle的innerHTML内容
+                    $dialogDisc.html("");//清空$dialogDisc的innerHTML内容
+                    $dialogKey.find(".key-tips").html("");//清空$key-tips的innerHTML内容
+                    key2 = 1;//拿到该线索，key2=1
+                    $("#key02").attr("data-tips", "您已经拿到该线索！");//设置该线索的自定义属性
+                    $mask.addClass("show");//显示点击查看按钮
+                });
+            }
+        } else if (dImg == 3) {//自定义属性dImg为03
+            if (dTips == "您已经拿到该线索！") {//已拿到该线索
+                dialogFlag = true;
+            } else {//该线索会话框首次显示
+                addShakeEvent();//调用
+            }
+        } else if (dImg == 4) {//自定义属性dImg为04
+            if (dTips == "您已经拿到该线索！") {//已拿到该线索
+                dialogFlag = true;
+                $(".key-img").addClass("result");//切换拿到线索的图片
+                $(".key-img span").hide();//需要点击的线索span隐藏
+                $dialogTitle.html("红白机上落了一层灰尘");//改变$dialogTitle的innerHTML内容
+                $dialogDisc.html("卡带插上去似乎有些接触不良");//改变$dialogDisc的innerHTML内容
+            } else {//该线索会话框首次显示
+                $(".key-img_04 span").on("touchstart", function() {//按下该线索span
+                    $dialogTitle.html("红白机上落了一层灰尘");//改变$dialogTitle的innerHTML内容
+                    $dialogDisc.html("卡带插上去似乎有些接触不良");//改变$dialogDisc的innerHTML内容
+                    setTimeout(function() {//设置延时定时器，1000ms后执行
+                        $(".key-img_04").addClass("result");//切换拿到线索的图片
+                        $(".key-img span").hide();//需要点击的线索span隐藏
+                        $dialogKey.find(".key-tips").html("");//清空$key-tips的innerHTML内容
+                        key4 = 1;//拿到该线索，key4=1
+                    }, 1e3);
+                    $("#key04").attr("data-tips", "您已经拿到该线索！");//设置该线索的自定义属性
+                    $mask.addClass("show");//显示点击查看按钮
+                });
+            }
+        }
+        $dialog.on("tap", function() {//点击会话框
+            if (dialogFlag) {
+                $(this).removeClass("show");//该会话框消失
+                $(".item").removeClass("after");//切换原始图片
+            }
+        });
+        $mask.on("tap", function() {//点击查看
+            $mask.removeClass("show");//点击查看消失
+            setTimeout(function() {//200ms后调用showResult
+                showResult();//显示获取线索的对应内容
+            }, 200);
+            $audioResult.play();//播放成功拿到线索音频
+        });
+    };
+    function shakeEventDidOccur() {
+        $(".key-img_03").addClass("shake");//添加抖动
+        $(".key-img_03").on("webkitAnimationEnd", function() {
+            var $this = $(this);
+            key3 = 1;//拿到该线索，key3=1
+            removeShakeEvent();//??
+            $this.removeClass("shake");//移出抖动
+            $("#key03").attr("data-tips", "您已经拿到该线索！");//设置该线索的自定义属性
+            $dialog.find(".dialog-mask").addClass("show");//显示点击查看按钮
+        });
+    }
+    var keyString = "";//用于连接密码
+    var openDoor = function() {
+        var $keystatus = $(".keystatus");
+        var $keystatusItem = $(".keystatus span");//输入密码
+        var $numItem = $(".num-item");//0-9按键
+        var $lockKeyClose = $(".lock-key_close");//取消
+        var flag = 0;//计算按下几次
+        $numItem.on("touchstart", function() {//按下0-9之间的数字
+            var value = $(this).attr("data-num");//按下数字
+            flag += 1;
+            if (flag > 4) {//输入超过4个
+                $keystatusItem.removeClass("active");//清空输入密码，移出背景
+                $keystatusItem.eq(0).addClass("active");//输入密码超过四次，第一个span添加背景颜色
+                keyString = value;//当前输入值
+                flag = 1;//超过4，flag从1计算
+                $keystatus.removeClass("wrong");//移出wrong,去除抖动样式
+            } else {//输入密码在4个以内包括4
+                $keystatusItem.eq(flag - 1).addClass("active");//对应密码位置加背景颜色
+                keyString += value;//输入密码
+            }
+            if (flag == 4) {//输入第四个密码
+                if (keyString == "0325") {//如果输入密码为0325
+                    $(".page-finished").addClass("show");//page-finished显示
+                    $(".page-finished p").eq(1).on("webkitAnimationEnd", function() {
+                        $("#pageKV").addClass("show");
+                    });
+                } else {//如果输入错误
+                    $keystatus.addClass("wrong");//输入密码抖动
+                }
+            }
+        });
+        $lockKeyClose.on("tap", function() {//点击取消
+            $(".lock-key").removeClass("show");//输入密码页面隐藏
+        });
+    };
+    openDoor();//执行输入密码函数
+    $("#jsTips").on("tap", function() {//点击右上角提示
+        showDialog($(this).attr("data-title"), $(this).attr("data-disc"));//传入提示参数
+    });
+});
---
+++ Room/js/loadding.js
@@ -0,0 +1,101 @@
+/* loading */
+"use strict";
+$(function() {
+    var PreLoad = function(resource, object) {
+        var obj = object || {};//有object就把object赋给obj,没有创建个对象赋给obj
+        this.source = resource;//加载资源
+        this.count = 0;//
+        this.total = resource.length;//加载资源总个数
+        this.onload = obj.onload;//
+        this.prefix = obj.prefix || "";
+        this.init();//调用init
+    };
+    PreLoad.prototype.init = function() {
+        var that = this;
+        //遍历加载资源
+        that.source.forEach(function(ele) {
+            var format = ele.substr(ele.indexOf(".") + 1).toLowerCase();//截取格式
+            var path = that.prefix + ele;//文件路径
+            switch (format) {
+                case "js"://截取格式为js调用PreLoad下的script方法，传入实参path路径
+                    that.script.call(that, path);
+                    break;
+                case "css":
+                    that.stylesheet.call(that, path);
+                    break;
+                case "svg":
+                case "jpg":
+                case "gif":
+                case "png":
+                case "jpeg":
+                    that.image.call(that, path);
+            }
+        });
+    };
+    PreLoad.prototype.image = function(path) {
+        var img = document.createElement("img");//创建img
+        this.load(img, path);//调用PreLoad下的方法，传入实参img,path
+        img.src = path;
+    };
+    PreLoad.prototype.stylesheet = function(path) {
+        var link = document.createElement("link");
+        this.load(link, path);
+        link.rel = "stylesheet";
+        link.type = "text/css";
+        link.href = path;
+        document.head.appendChild(link);
+    };
+    PreLoad.prototype.script = function(path) {
+        var Script = document.createElement("script");
+        this.load(Script, path);
+        Script.type = "text/javascript";
+        Script.src = path;
+        document.head.appendChild(Script);
+    };
+    PreLoad.prototype.load = function(file, path) {
+        var that = this;
+        //加载完成
+        file.onload = file.onerror = file.onabort = function(file) {
+            // 如果有PreLoad下有onload属性就执行onload，传对象实参过去
+            that.onload && that.onload({
+                count: ++that.count,//当前加载多少个资源文件
+                total: that.total,//加载资源总数
+                item: path,//加载资源路径
+                type: file.type//加载资源类型
+            });
+        };
+    };
+    var resources = [ "style/index.css","js/touch.js"];
+    var images = [ "images/img/light/bg2_01.jpg", "images/img/light/bg2_02.jpg", "images/img/light/bg2_03.jpg", "images/img/light/bg2_04.jpg", "images/img/light/bg2_05.jpg", "images/img/dark/bg2_01.jpg", "images/img/dark/bg2_02.jpg", "images/img/dark/bg2_03.jpg", "images/img/dark/bg2_04.jpg", "images/img/dark/bg2_05.jpg", "images/img/flash/img_01.png", "images/img/flash/img_02.png", "images/img/flash/img_03.png", "images/img/flash/img_04.png", "images/img/key/key01.png", "images/img/key/key01_2.png", "images/img/key/key02.png", "images/img/key/key02_2.png", "images/img/key/key03.png", "images/img/key/key04.png", "images/img/key/key04_2.png", "images/img/key/key05.png", "images/img/key/key06.png", "images/img/key/key07.png", "images/img/key/key08.png", "images/img/key/key09.png", "images/img/loading/loading01.png", "images/img/loading/loading02.png" ];
+    resources = resources.concat(images);
+    new PreLoad(resources, {
+        onload: function(load) {
+            var count = load.count;//当前加载多少个资源文件
+            var total = load.total;//加载资源总数
+            var percent = Math.ceil(100 * count / total);//当前加载占总加载资源的百分比
+            $("#percent").html(percent + "%");//设置$("#percent")的内容
+            setTimeout(function() {//设置延迟定时器，500ms后执行
+                $("#audioBg")[0].play();//开始播放当前的音频
+                // $("#jsBgBox").removeClass("dark");
+                if (count == total) {//加载资源完毕
+                    var $load = $("#loading");//获取loading
+                    $("#percent").hide();//加载百分比隐藏
+                    $("#loading .animate-item").addClass("loaded");//class属性添加loaded
+                    setTimeout(function() {//设置延迟定时器，2500ms后执行
+                        $load.find(".text").addClass("show");//class属性添加show,text显示
+                        setTimeout(function() {//设置延迟定时器，2500ms后执行
+                            $load.find(".tips").addClass("show");//class属性添加show,tips显示
+                            //添加点击事件
+                            $load.find(".animate-item").on("tap", function() {
+                                $($load).addClass("compvare");//添加compvare，背景颜色由黑变白，放大2倍
+                                setTimeout(function() {
+                                    $load.remove();//删除$load节点
+                                }, 1e3);
+                            });
+                        }, 2500);
+                    }, 2500);
+                }
+            }, 500);
+        }
+    });
+});
